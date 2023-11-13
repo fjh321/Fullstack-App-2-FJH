b6b23d43715dac4d95b3f54a6307d8c7c4020ee1
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-11-12 23:46:03 -0500

    troubleshooting database.js errors

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'url': 'mongodb+srv://fernandojhernandez123:vXy4RGR0VNQS67wo@cluster0.5vfzb42.mongodb.net/test?retryWrites=true&w=majority',
+    'dbName': 'demo'
+};
+
+// vXy4RGR0VNQS67wo
